88b85936c57b193e3601faf21a992d0895d3689d
Alps360 <h.alpsarioglu@gmail.com>
2025-10-24 14:09:52 +0300

    asd

---
+++ src/MainBook.java
@@ -0,0 +1,9 @@
+public class MainBook {
+    public static void main(String[] args) {
+        Book b1 = new Book();
+        Book b2 = new Book("Art of War", "Sun Tzu", 50.00);
+
+        b1.showDetails();
+        b2.showDetails();
+    }
+}
